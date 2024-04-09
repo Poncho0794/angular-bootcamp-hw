37186bb6c6975c8f5dcd3ab194dd253350ac3371
Alfonso Banda <benbanda07@gmail.com>
2024-04-08 23:28:26 -0600

    complete Homework 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { Homework2Component } from './components/homework2/homework2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    Homework2Component
   ],
   imports: [
     BrowserModule,

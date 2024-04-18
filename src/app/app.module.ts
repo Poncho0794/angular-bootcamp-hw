ec0510e9e0bbf7d0fb781b76b06e9f34f67b5a11
Alfonso Banda <benbanda07@gmail.com>
2024-04-18 12:21:24 -0600

    Complete Homework 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { Homework2Component } from './components/homework2/homework2.component';
+import { Homework3Component } from './components/homework3/homework3.component';
+import { CapitalizeDirective } from './directives/capitalize-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    Homework2Component
+    Homework2Component,
+    Homework3Component,
+    CapitalizeDirective
   ],
   imports: [
     BrowserModule,

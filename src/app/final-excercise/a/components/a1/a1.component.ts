ad3770f966aeb6d671049d9c1d16b04642d2953d
Alfonso Banda <benbanda07@gmail.com>
2024-06-03 02:52:48 -0600

    Add search box

--- src/app/final-excercise/a/components/a1/a1.component.ts
+++ src/app/final-excercise/a/components/a1/a1.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { IItem, ItemsToSell } from '../../../z/utils/ItemsToSell';
 import { StorageService } from '../../services/storage.service';
-import { map } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-a1',
@@ -13,16 +13,39 @@ export class A1Component {
   ItemsToSell: IItem[] = [];
   discountFilter: boolean = false;
   itemSubject: BehaviorSubject<IItem[]>;
+
+  @ViewChild('searchBox', { static: true })
+  searchBox!: ElementRef;
+
   constructor(private itemService: StorageService) {
     this.itemSubject = this.itemService.getSubject();
     this.itemSubject.subscribe((items) => {
       this.ItemsToSell = items;
     });
   }
+  ngOnInit() {
+    // se hace en este evento porque necesito que exista el input,
+    // se crea un observable a partir del evento de escribir
+    fromEvent(this.searchBox.nativeElement, 'input')
+      .pipe(
+        map((event: any) => event.target.value),
+        debounceTime(1000)
+      )
+      .subscribe((value) => {
+        this.filterByName(value);
+      });
+  }
   filterDiscountedItems() {
     !this.discountFilter
       ? this.filterItems(null)
       : this.filterItems((item) => !!item.offerDiscount);
+  }
+  filterByName(name: string) {
+    if (name.length)
+      this.filterItems((item) =>
+        item.title.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+      );
+    else this.filterItems(null);
   }
   filterItems(criteriaCB: ((item: IItem) => boolean) | null) {
     if (criteriaCB) {
